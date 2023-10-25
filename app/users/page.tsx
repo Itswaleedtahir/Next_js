329c570ee6e5fc67f0577cf461a77557993c2a5b
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-10-25 11:46:13 +0500

    Update page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 interface Users {
   id: number;
   name: string;
+  email: string;
 }
 const UsersPage =async  () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache:'no-store'}) //caching is only supported in Fetch
@@ -9,10 +10,20 @@ const UsersPage =async  () => {
   return (
     <>
     <h1>Users</h1>
-    <p>{new Date().toLocaleTimeString()}</p>
-    <ul>
-      {users.map((user)=><li key={user.id}>{user.name}</li>)}
-    </ul>
+    <table className='table table-bordered'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+      {users.map((user)=><tr key={user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+      </tr>)}
+      </tbody>
+    </table>
     </>
   )
 }
