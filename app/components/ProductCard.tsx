684bcedbe94c75d1e2437240085f427bcc89ab76
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-10-25 11:08:28 +0500

    Update ProductCard.tsx

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import AddToCart from './AddToCart'
 
 const ProductCard = () => {
+  //tailwind styling used here
   return (
-    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-700'>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-700'> 
         <AddToCart />
     </div>
   )
