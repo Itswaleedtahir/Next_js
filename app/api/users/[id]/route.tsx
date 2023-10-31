d148b32b9c7c54dd8fe68cb094dfa4f967739cf0
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-10-31 11:38:53 +0500

    Update route.tsx

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -6,3 +6,10 @@ export function GET(request: NextRequest, {params}:{params:{id:number}}){
     }
     return NextResponse.json({id:1,name:'Waleed'})
 }
+
+export async function PUT(request:NextRequest,{params}:{params:{id:number}}){
+    const body =await request.json()
+    if(!body.name) return NextResponse.json({error:"Name is required"},{status:400})
+    if(params.id>10) return NextResponse.json({error:"User Not Found"},{status:400})
+    return NextResponse.json({id:1,name:body.name},)
+}
