3fa6bdfe75cd9880dd84271edcc6056d98cecb8d
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-10-31 11:16:13 +0500

    Update route.tsx

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,6 +1,6 @@
 import {NextRequest,NextResponse} from "next/server"
 
-export function GET(){
+export function GET(request: NextRequest){
    return NextResponse.json([
     {id:1,name:"Waleed"},
     {id:2,name:"Usman"}
